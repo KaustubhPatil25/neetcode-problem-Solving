0bcab1033fe494135b0b0063af6522ae5406705e
Kaustubh Satish Patil <97739923+KaustubhPatil25@users.noreply.github.com>
2026-05-08 00:56:49 -0230

    error

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
@@ -2,17 +2,17 @@ public class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> count = new HashMap<>();
         for (int num : nums) {
-            count.put(num, count.getOrDefault(num, 1) + 1); // Error 1
+            count.put(num, count.getOrDefault(num, 1) + 1); 
         }
 
         List<int[]> arr = new ArrayList<>();
         for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-            arr.add(new int[] {entry.getKey(), entry.getValue()}); // Error 2
+            arr.add(new int[] {entry.getKey(), entry.getValue()}); 
         }
-        arr.sort((a, b) -> a[0] - b[0]); // Error 3
+        arr.sort((a, b) -> a[0] - b[0]); 
 
         int[] res = new int[k];
-        for (int i = 0; i <= k; i++) { // Error 4
+        for (int i = 0; i <= k; i++) { 
             res[i] = arr.get(i)[1];
         }
         return res;
